6c01ab5c9ed5c6d396069656975dd3cee8458a51
logeishp <143477590+logeishp@users.noreply.github.com>
2023-08-29 00:07:00 -0500

    Update streamlit_app.py
    added emoji
    added a line
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('BFast Menu')
-streamlit.text('Oatmeal')
-streamlit.text('Kale')
-streamlit.text('Egg')
+streamlit.text('🥣 Oatmeal')
+streamlit.text('🥗 Kale')
+streamlit.text('🐔 Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
