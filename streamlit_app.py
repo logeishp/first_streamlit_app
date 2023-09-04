69fb6bea6e2394dceecfb63c2d296bc94745f830
logeishp <143477590+logeishp@users.noreply.github.com>
2023-09-03 19:37:32 -0500

    Update streamlit_app.py
    break url to base url and search term

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
 # normalize the json version of the response
