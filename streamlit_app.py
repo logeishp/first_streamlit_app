2592b59dbc0d4f7c70d4652177c43b0ef43f7a17
logeishp <143477590+logeishp@users.noreply.github.com>
2023-09-03 19:40:53 -0500

    Update streamlit_app.py
    pass fruit name thru variable to the url

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
 # normalize the json version of the response
